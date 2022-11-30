6f712ae2bf5ad6a704efa8c5ba81ca12af5410dd
Chaitanya Sase <chaitanya2sase@gmail.com>
2022-11-30 10:22:29 +0530

    new

--- client/src/detail/details/Details.js
+++ client/src/detail/details/Details.js
@@ -88,16 +88,16 @@ export default function Details (props) {
   </div>
   <div>
        
-        <Popup trigger={<button> Contact to Owner</button>} position="right center">
+        {/* <Popup trigger={<button> Contact to Owner</button>} position="right center">
           
         <div>{users.mobile_num} 
         
         </div>
         
         </Popup>
-        
+         */}
   </div>
-  <div><a href= "tel:+919764198037">call Me </a></div>
+  <div><a href= "tel:+919764198037"><button id="bt" >Click</button> </a></div>
         
   <div className='title1'>
     <b className='t2'>{users.owner_name}</b>
